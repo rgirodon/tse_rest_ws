c7501ed7a43afcaf339c9f39481f8ac549813a1d
rgirodon <remy.girodon@gmail.com>
2024-10-13 19:21:54 +0200

    Remove lombok

--- src/main/java/org/rygn/firstrestws/Employee.java
+++ src/main/java/org/rygn/firstrestws/Employee.java
@@ -5,9 +5,7 @@ package org.rygn.firstrestws;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
-import lombok.Data;
 
-@Data
 @Entity
 public class Employee {
 
@@ -26,4 +24,35 @@ public class Employee {
 		this.name = name;
 		this.role = role;
 	}
+	
+	@Override
+	public String toString() {
+		return "Employee [id=" + id + ", name=" + name + ", role=" + role + "]";
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getRole() {
+		return role;
+	}
+
+	public void setRole(String role) {
+		this.role = role;
+	}
+	
+	
 }
--- src/main/java/org/rygn/firstrestws/LoadDatabase.java
+++ src/main/java/org/rygn/firstrestws/LoadDatabase.java
@@ -1,23 +1,28 @@
 package org.rygn.firstrestws;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import lombok.extern.slf4j.Slf4j;
-
 
 @Configuration
-@Slf4j
 public class LoadDatabase {
+	
+	private Logger log;
+	
+	public LoadDatabase() {
+		this.log = LoggerFactory.getLogger(LoadDatabase.class);
+	}
 
 	@Bean
 	CommandLineRunner initDatabase(EmployeeRepository repository) {
 		return args -> {
 			
-			log.info("Preloading " + repository.save(new Employee("Bilbo Baggins", "maçon")));
+			this.log.info("Preloading " + repository.save(new Employee("Bilbo Baggins", "maçon")));
 			
-			log.info("Preloading " + repository.save(new Employee("Frodo Baggins", "infirmier")));
+			this.log.info("Preloading " + repository.save(new Employee("Frodo Baggins", "infirmier")));
 		};
 	}
 }
